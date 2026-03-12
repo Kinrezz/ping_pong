8ba9ba7d44b4f2b72b9886a223133fc11e6c6a02
Kinriezz <veronika1402020@gmail.com>
2026-03-12 20:49:00 +0300

    one more

--- ping pong.py
+++ ping pong.py
@@ -28,8 +28,8 @@ class Player(GameSprite):
         if keys_pressed[K_s] and self.rect.y < 365:
             self.rect.y += self.speed
 class Ball(GameSprite):
-    speed_x = 3
-    speed_y = 3
+    speed_x = 5
+    speed_y = 5
     def update(self):
         if ball.rect.y > 450 or ball.rect.y < 0:
             Ball.speed_y *= -1
@@ -50,14 +50,26 @@ racket1 = Player('racket.png', 5, 200, 7, 39, 136)
 racket2 = Player('racket.png', 650, 200, 7, 39, 136)
 ball = Ball('tenis_ball.png', 250, 250, 7, 50, 60)
 #цикл
-win = font.Font(None, 60).render('Ура победа', True, (40,240,40))
-lose = font.Font(None, 60).render('Поражение', True, (240,40,40))
+win1 = font.Font(None, 60).render('игрок 2 лошпед)))))))', True, (40,240,40))
+win2 = font.Font(None, 60).render('игрок 1 лошпед)))))))', True, (240,40,40))
+is_collide_ball = False
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     if finish == False:
+        if is_collide_ball == True and not ( sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball)):
+            is_collide_ball = False
+        if ( sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball)) and is_collide_ball == False:
+            Ball.speed_x *= -1
+            is_collide_ball = True
         window.fill(wc)
+        if ball.rect.x <= -50:
+            window.blit(win2, (220, 220))
+            finish = True
+        if ball.rect.x >= 750:
+            window.blit(win1, (220, 220))
+            finish = True
         racket1.update_1()
         racket2.update_2()
         racket1.reset()
